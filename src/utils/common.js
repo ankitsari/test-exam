f143fb2a34180ceed572e7c8a146c000b462b33a
ankit <ankit.sari@gmail.com>
2018-06-20 15:15:04 +0530

    Update callback url

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,2 +1,2 @@
-export const  urlDomain = "https://innroad-exam.herokuapp.com"
-//export const  urlDomain = "http://localhost:3000"
+//export const  urlDomain = "https://innroad-exam.herokuapp.com"
+export const  urlDomain = "http://techview.azurewebsites.net/Account/OnAuthComplete"
